92a2eab9fe026467069112fa3d7e96280daedf6e
Aakanksha1901 <124653368+Aakanksha1901@users.noreply.github.com>
2026-01-23 10:32:18 +0530

    Add files via upload

---
+++ Transactions.sql
@@ -0,0 +1,48 @@
+create database BankTransfer;
+use BankTransfer;
+
+create table bank_accounts (
+account_id INT,
+customer_name VARCHAR(50),
+balance INT 
+);
+
+INSERT INTO bank_accounts VALUES
+(1, 'Ravi', 5000),
+(2, 'Anita', 3000);
+
+select * from bank_accounts;
+
+-- Atomicity (All or Nothing)
+-- Transaction
+
+START TRANSACTION;
+
+UPDATE bank_accounts
+SET balance = balance - 1000
+where account_id = 1;
+
+UPDATE bank_accounts
+SET balance = balance + 1000
+WHERE account_id = 2;
+
+COMMIT;
+
+-- Consistency (Rules Always True)
+-- 🔹 Rule
+-- Balance cannot be negative
+UPDATE bank_accounts
+SET balance = - 500
+WHERE account_id = 1;
+
+
+-- Isolation (No Dirty Reads)
+-- Scenario
+-- Transaction A is transferring money
+-- Transaction B checks balance before commit
+
+START TRANSACTION;
+UPDATE bank_accounts SET balance = balance - 2000 where account_id = 1;
+
+-- Durability (Once committed, Always Saved)
+COMMIT;
